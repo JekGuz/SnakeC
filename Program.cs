5980b1c8d42dd8c6e1b27ba74410ab1494976256
«JekGuz» <jekaterina.guzek@gmail.com>
2025-04-14 11:11:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            draw(x2, y2, sym2);
+
+            Console.ReadLine();
+        }
+        // Для вывода на экран точеку с помощью кардинат и символа
+        // Скрывает, как конкретно выводит на экран символ
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        }
+}
