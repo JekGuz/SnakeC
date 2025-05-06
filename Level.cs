89b74f850412c51db7be5b23b1b70c0b1955e520
«JekGuz» <jekaterina.guzek@gmail.com>
2025-05-06 12:03:13 +0300

    commit

--- Level.cs
+++ Level.cs
@@ -11,26 +11,26 @@ namespace SnakeC
         public int level { get; private set; } = 1;
         public int Speed { get; private set; } = 100;
         public ConsoleColor SnakeColor { get; private set; } = ConsoleColor.White;
-        public ConsoleColor FoodColor { get; private set; } = ConsoleColor.Red;
+        public ConsoleColor FoodColor { get; private set; } = ConsoleColor.White;
 
         public void UpdateLevel(int score)
         {
-            if (score >= 10)
+            if (score >= 10)  // сильно ускоряется и желтая
             {
                 level = 4;
                 Speed = 40;
                 SnakeColor = ConsoleColor.Yellow;
                 FoodColor = ConsoleColor.Yellow;
             }
-            else if (score >= 6)
+            else if (score >= 6)  // ускоряется сильнее и цвет зелеый после 6
             {
                 level = 3;
                 Speed = 60;
                 SnakeColor = ConsoleColor.Green;
-                FoodColor = ConsoleColor.White;
+                FoodColor = ConsoleColor.Green;
 
             }
-            else if (score >= 3)
+            else if (score >= 3) // далее сьест сколько то
             {
                 level = 2;
                 Speed = 80;
@@ -39,10 +39,10 @@ namespace SnakeC
             }
             else
             {
-                level = 1;
+                level = 1;  // начало
                 Speed = 100;
                 SnakeColor = ConsoleColor.White;
-                FoodColor = ConsoleColor.Red;
+                FoodColor = ConsoleColor.White;
             }
         }
     }
